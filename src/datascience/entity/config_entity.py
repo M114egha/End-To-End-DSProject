484284b60cf0d3e1a70cf029b039d013fc6a9c83
Meghana Bhairi <meghabhairi114@gmail.com>
2025-06-03 14:51:35 +0530

    DataValidationDone

--- src/datascience/entity/config_entity.py
+++ src/datascience/entity/config_entity.py
@@ -2,8 +2,16 @@ from dataclasses import dataclass
 from pathlib import Path
 
 @dataclass
-class DataIngestionConfig:
+class DataIngestionConfig: 
     root_dir: Path
     source_url: str
     local_data_file: Path
     unzip_dir: Path
+
+
+@dataclass
+class DataValidationConfig:
+    root_dir:Path
+    STATUS_FILE: str
+    unzip_data_dir:Path
+    all_schema:dict
